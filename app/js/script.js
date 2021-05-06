f3d523a6853e223a5b74a16b2827cce1938cc443
Ajaydip Singh <singh@minerva.kgi.edu>
2021-05-06 15:05:15 +0300

    :lipstick: style: animate hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,19 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener("click", function () {
   console.log("open hamburger");
 
-  if (btnHamburger.classList.contains("open")) {
-    btnHamburger.classList.remove("open");
+  if (header.classList.contains("open")) {
+    // Close the hamburger menu
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
   } else {
-    btnHamburger.classList.add("open");
+    // Open the hamburger menu
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
   }
 });
